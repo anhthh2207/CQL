0aa930abfbf7ba1a643b2dbe7e3341626943ac6e
21anh.thh <21anh.thh@vinuni.edu.vn>
2023-06-29 22:48:07 +0700

    conv online net

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,5 @@
+
+
 import gym
 import math
 # import pybullet_envs
@@ -10,12 +12,14 @@ from buffer import ReplayBuffer
 import glob
 from utils import save, collect_random
 import random
-from agent import CQLAgent, CQLAgent_Online
+from data import Data
+from agent import CQLAgent
+from preprocess import AtariEnv
 
 def get_config():
     parser = argparse.ArgumentParser(description='RL')
     parser.add_argument("--run_name", type=str, default="CQL-DQN", help="Run name, default: CQL-DQN")
-    parser.add_argument("--env", type=str, default="Breakout-v4", help="Gym environment name, default: Breakout-v4")
+    parser.add_argument("--env", type=str, default="Breakout-v0", help="Gym environment name, default: Breakout-v0")
     parser.add_argument("--episodes", type=int, default=210, help="Number of episodes, default: 200")
     parser.add_argument("--buffer_size", type=int, default=100_000, help="Maximal training dataset size, default: 100_000")
     parser.add_argument("--seed", type=int, default=1, help="Seed, default: 1")
@@ -27,11 +31,12 @@ def get_config():
     args = parser.parse_args()
     return args
 
-def train(config):
+def eval(config):
     np.random.seed(config.seed)
     random.seed(config.seed)
     torch.manual_seed(config.seed)
     env = gym.make(config.env)
+    # env = AtariEnv(game='Breakout')
     
     env.seed(config.seed)
     env.action_space.seed(config.seed)
@@ -45,42 +50,43 @@ def train(config):
     total_steps = 0
     
     print(env.observation_space.shape)
-    agent = CQLAgent_Online(state_size=env.observation_space.shape,
+    # agent = CQLAgent(state_size=84*84,
+    #                     action_size=env.action_space.n,
+    #                     device=device)
+    agent = CQLAgent(state_size=math.prod(env.observation_space.shape),
                         action_size=env.action_space.n,
                         device=device)
-    
+    agent.network.load_state_dict(torch.load('trained_models/breakout_online.pt'))
+    agent.network.eval()
 
 
-    buffer = ReplayBuffer(buffer_size=config.buffer_size, batch_size=32, device=device)
-    
-    collect_random(env=env, dataset=buffer, num_samples=10000)
+    # collect_random(env=env, dataset=buffer, num_samples=10000)
     
     if config.log_video:
         env = gym.wrappers.Monitor(env, './video', video_callable=lambda x: x%10==0, force=True)
 
     for i in range(1, config.episodes+1):
+        # perform with the learned model
         state = env.reset()
-        episode_steps = 0
         rewards = 0
+        episode_steps = 0
         while True:
             action = agent.get_action(state, epsilon=eps)
             steps += 1
             next_state, reward, done, _ = env.step(action[0])
-            buffer.add(state, action, reward, next_state, done)
-            loss, cql_loss, bellmann_error = agent.learn(buffer.sample())
             state = next_state
             rewards += reward
             episode_steps += 1
             eps = max(1 - ((steps*d_eps)/config.eps_frames), config.min_eps)
+            env.render()
             if done:
                 break
-
         
 
         average10.append(rewards)
         total_steps += episode_steps
-        print("Episode: {} | Reward: {} | Q Loss: {} | Steps: {}".format(i, rewards, loss, steps,))
-        
+        print("Episode: {} | Reward: {} | Steps: {}".format(i, rewards, steps,))
+        # print("Episode: {} | Reward: {} | Steps: {}".format(i, reward_, steps,))
 
         if (i %10 == 0) and config.log_video:
             mp4list = glob.glob('video/*.mp4')
@@ -89,4 +95,4 @@ def train(config):
 
 if __name__ == "__main__":
     config = get_config()
-    train(config)
+    eval(config)
--- networks.py
+++ networks.py
@@ -9,6 +9,7 @@ class DDQN(nn.Module):
         self.action_size = action_size
         self.flatten = nn.Flatten()
         self.head_1 = nn.Linear(self.input_shape, layer_size)
+        
         self.ff_1 = nn.Linear(layer_size, layer_size)
         self.ff_2 = nn.Linear(layer_size, action_size)
 
@@ -20,5 +21,38 @@ class DDQN(nn.Module):
         x = torch.relu(self.head_1(input))
         x = torch.relu(self.ff_1(x))
         out = self.ff_2(x)
+        
+        return out
+
+class DDQN_Online(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(DDQN_Online, self).__init__()
+
+        self.input_shape = state_size
+        self.action_size = action_size
+        self.conv = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1, padding=0),
+                                    nn.ReLU(),
+                                    nn.MaxPool2d(kernel_size=4, stride=2),
+                                    nn.Conv2d(in_channels=6, out_channels=10, kernel_size=5, stride=1, padding=0),
+                                    nn.ReLU(),
+                                    nn.MaxPool2d(kernel_size=4, stride=4),
+                                    nn.Conv2d(in_channels=10, out_channels=14, kernel_size=5, stride=1, padding=0),
+                                    nn.ReLU(),
+                                    nn.Flatten())
+
+        self.ff_1 = nn.Linear(3920, 1240)
+        self.ff_2 = nn.Linear(1240, 512) 
+        self.ff_3 = nn.Linear(512, 64)
+        self.ff_4 = nn.Linear(64, action_size)
+
+    def forward(self, input):
+        """
+        
+        """
+        x = self.conv(input)
+        x = torch.relu(self.ff_1(x))
+        x = torch.relu(self.ff_2(x))
+        x = torch.relu(self.ff_3(x))
+        out = self.ff_4(x)
         
         return out
